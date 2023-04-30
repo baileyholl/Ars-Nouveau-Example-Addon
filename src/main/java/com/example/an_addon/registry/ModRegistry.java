cdc3c54405eb5ccb1b70fc52461e82a0cc2fcb9e
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-04-30 22:51:32 +0200

    Update base ars. Added SpellSound Example with random sound

--- src/main/java/com/example/an_addon/registry/ModRegistry.java
+++ src/main/java/com/example/an_addon/registry/ModRegistry.java
@@ -2,6 +2,9 @@ package com.example.an_addon.registry;
 
 import com.example.an_addon.item.ExampleCosmetic;
 import com.hollingsworth.arsnouveau.ArsNouveau;
+import com.hollingsworth.arsnouveau.api.sound.SpellSound;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -15,17 +18,29 @@ public class ModRegistry {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
 
 
     public static void registerRegistries(IEventBus bus){
         BLOCKS.register(bus);
         ITEMS.register(bus);
+        SOUNDS.register(bus);
     }
 
     public static final RegistryObject<Item> EXAMPLE;
 
+    //this is an example of how to register a sound. You also need to add the sound to the sound.json file, referencing your ogg files, and a texture for the button under textures/sounds.
+    //this example will use one of the existing sounds randomly
+    public static RegistryObject<SoundEvent> EXAMPLE_FAMILY = SOUNDS.register("example_sound", () -> makeSound("example_sound"));
+    public static SpellSound EXAMPLE_SPELL_SOUND;
+
+
     static {
         EXAMPLE = ITEMS.register("star_hat", () -> new ExampleCosmetic(new Item.Properties().tab(ArsNouveau.itemGroup)));
+    }
+
+    static SoundEvent makeSound(String name) {
+        return new SoundEvent(new ResourceLocation(MODID, name));
     }
 
 }
